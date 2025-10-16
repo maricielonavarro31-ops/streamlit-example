b5d1cdf4ff4af5a2acc32b290669acb3ec274c33
maricielonavarro31-ops <maricielonavarro31@gmail.com>
2025-10-15 18:53:17 -0600

    Create dashboardVentas2025.py

---
+++ dashboardVentas2025.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+# Process data for top selling products
+product_sales = df_excel.groupby('Product Name')['Sales'].sum().reset_index()
+product_sales = product_sales.sort_values(by='Sales', ascending=False)
+top_n_products = product_sales.head(5)
+
+# Create bar chart for top selling products using Plotly Express and Streamlit
+fig_sales = px.bar(top_n_products, x='Product Name', y='Sales', title='Top 5 Selling Products')
+fig_sales.update_layout(xaxis_tickangle=-45)
+st.plotly_chart(fig_sales)
+
+# Process data for top profitable products
+product_profit = df_excel.groupby('Product Name')['Profit'].sum().reset_index()
+product_profit = product_profit.sort_values(by='Profit', ascending=False)
+top_n_profitable_products = product_profit.head(5)
+
+# Create bar chart for top profitable products using Plotly Express and Streamlit
+fig_profit = px.bar(top_n_profitable_products, x='Product Name', y='Profit', title='Top 5 Most Profitable Products')
+fig_profit.update_layout(xaxis_tickangle=-45)
+st.plotly_chart(fig_profit)
