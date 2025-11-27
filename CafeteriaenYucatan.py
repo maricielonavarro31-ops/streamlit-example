ee38dab8d5b0b75f54aa0803301d45f938af78dc
maricielonavarro31-ops <maricielonavarro31@gmail.com>
2025-11-26 22:26:33 -0600

    Update CafeteriaenYucatan.py

--- CafeteriaenYucatan.py
+++ CafeteriaenYucatan.py
@@ -111,11 +111,6 @@ if not filtered_df.empty:
 else:
     st.warning("No store locations to display based on current filters.")
     
-# Create the pydeck Deck
-r = pdk.Deck(
-    layers=[geojson_layer],
-    initial_view_state=view_state,
-   
 st.subheader("Filtered Data Table")
 if not filtered_df.empty:
     st.dataframe(filtered_df)
